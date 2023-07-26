bee17bf5aa60e000a742c9306cccedf0dbdd07e0
Loki99911 <nikita_kresik@i.ua>
2023-07-26 16:23:09 +0300

    Update MetaMaskInstallPrompt.styled.js

--- src/components/MetaMaskInstallPrompt/MetaMaskInstallPrompt.styled.js
+++ src/components/MetaMaskInstallPrompt/MetaMaskInstallPrompt.styled.js
@@ -1,9 +1,13 @@
 import styled from "styled-components";
 export const MetaMaskInstallPromptWrapper = styled.div`
   display: flex;
-  background-color: var(--main-opacity);
+  background-color: var(--second-opacity);
   flex: 1;
   justify-content: center;
   align-items: center;
   width: 100%;
+  a{
+    display: block;
+    text-align: center;
+  }
 `;
